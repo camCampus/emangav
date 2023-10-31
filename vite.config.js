99c66cffda148e7ff4418aed421120e39b9536c5
camCampus <camille.bochard@le-campus-numerique.fr>
2023-10-31 16:11:15 +0100

    Vercel fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import {resolve} from 'path';
 import {defineConfig} from "vite";
 
-// export default defineConfig ({
-//
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//         nested: resolve(__dirname, 'nested/index.html'),
-//       }
-//     }
-//   }
-// });
-
 export default defineConfig({
     root: resolve(__dirname, 'src'),
+    build: {
+        outDir: '../dist'
+    },
 })
