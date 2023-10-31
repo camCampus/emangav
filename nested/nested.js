e999b31dad67c15cd5b5389140bcdc19c2be18e2
camCampus <camille.bochard@le-campus-numerique.fr>
2023-10-31 16:08:54 +0100

    Burger menu fix

--- nested/nested.js
+++
@@ -1,2 +0,0 @@
-// Import our custom CSS for index.html
-import "../scss/nested.scss";
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
+import {resolve} from 'path';
+import {defineConfig} from "vite";
 
-import { resolve } from 'path';
-import { defineConfig } from "vite";
+// export default defineConfig ({
+//
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         nested: resolve(__dirname, 'nested/index.html'),
+//       }
+//     }
+//   }
+// });
 
-export default defineConfig ({
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html'),
-      }
-    }
-  }
-});
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+})
